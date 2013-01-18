95e34fa62709fd3b978ce828c2390e191624da0b
alzuin <alberto@azns.it>
2013-01-18 17:45:12 +0100

    updated schema.rb now compatible with Devise 2.1

--- lib/devise_imapable/schema.rb
+++ lib/devise_imapable/schema.rb
@@ -1,15 +1,21 @@
-Devise::Schema.class_eval do
+require 'devise/version'
 
-    # Creates email
-    #
-    # == Options
-    # * :null - When true, allow columns to be null.
-    # * :default - Should be set to "" when :null is false.
-    def imap_authenticatable(options={})
-      null = options[:null] || false
-      default = options[:default] || ""
+# Devise 2.1 removes schema stuff
+if Devise::VERSION < "2.1"
+  require 'devise/schema'
+  Devise::Schema.class_eval do
 
-      apply_schema :email, String, :null => null, :default => default
+      # Creates email
+      #
+      # == Options
+      # * :null - When true, allow columns to be null.
+      # * :default - Should be set to "" when :null is false.
+      def imap_authenticatable(options={})
+        null = options[:null] || false
+        default = options[:default] || ""
+
+        apply_schema :email, String, :null => null, :default => default
+      end
+
     end
-
 end
